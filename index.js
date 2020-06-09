c1a9ca5cbd61efb691dcd2ef8afbc83a1a588b2a
Segy Hendro Pratama <segy.hendro@mekari.com>
2020-06-09 19:20:01 +0700

    feat: setup test page for parcel

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = () => {
+    return(
+        <div>Hello</div>
+    );
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('app'));
